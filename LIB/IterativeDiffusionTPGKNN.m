2abeb8d9412ea0b3da6e995b74e14e8efb464e6d
lqmmring <lqmmring@163.com>
2021-05-22 12:03:41 +0800

    Update IterativeDiffusionTPGKNN.m

--- LIB/IterativeDiffusionTPGKNN.m
+++ LIB/IterativeDiffusionTPGKNN.m
@@ -1,10 +1,7 @@
 function [WW_G]=IterativeDiffusionTPGKNN(W,K, viewN)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% This code is an implementation of the diffusion process in the paper:
-%%% "Affinity Learning via a Diffusion Process for Subspace Clustering"
-%%% By QILIN LI (li.qilin@postgrad.curtin.edu.au)
-%%% Last Update 05/07/2018
+%%% This code is an implementation of the diffusion process.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% Pre-processing of affinity matrix W
